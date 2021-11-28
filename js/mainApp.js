d5dd3cebfd5bc608979b812c13068189ead8ba4c
jgard2019 <jwgardner22@gmail.com>
2021-11-27 23:33:04 -0500

    start js stuff

---
+++ js/mainApp.js
@@ -0,0 +1,8 @@
+//attempting to rebuild singleprojects and main via js
+
+function clearChildrenElements() {
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
+};
+
