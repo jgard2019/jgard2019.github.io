5b89c70069d0d746ee39938f8f73aeaecfd92f1b
jgard2019 <jwgardner22@gmail.com>
2021-12-14 16:20:26 -0500

    revamping

--- js/mainApp.js
+++ js/mainApp.js
@@ -1,8 +1,16 @@
-//attempting to rebuild singleprojects and main via js
+const anchorContainerEl = document.querySelector(".anchor-container");
 
-function clearChildrenElements() {
+function clearChildrenElements(element) {
     while (element.firstChild) {
         element.removeChild(element.lastChild);
     }
 };
 
+//add listeners
+const vpLink = document.querySelector(".vPetLink");
+vpLink.addEventListener("click", () =>{
+    window.location.href = "./virtualPet.html";
+    //window.location.replace("");
+});
+
+export {clearChildrenElements}
